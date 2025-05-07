bfa6e3f1cfa23fe445a142e71da9612600778655
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-07 22:38:51 +0530

    Add sign-up component

--- src/app/auth/components/sign-up/sign-up.component.ts
+++ src/app/auth/components/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  user= {
+    userName:'',
+    firstName:'',
+    lastName:'',
+    dob:'',
+    email:'',
+    password:''
+  };
 
+  constructor(private http: HttpClient) { }
+
+  onSubmit(form: HTMLFormElement, ngForm: NgForm){
+    if (ngForm.invalid) {
+      const elm = <HTMLInputElement>form.querySelector('.ng-invalid');
+      elm.focus();
+      elm.select();
+    } else{
+      if(this.validateForm()){
+        this.http.post('http://localhost:8080/app/user', this.user, {responseType: 'text'}).subscribe({
+          next: res => {
+            console.log(res);
+          },
+          error: err => {
+            console.log(err)
+          }
+        });
+        ngForm.reset();
+      } else {
+        console.log("Form invalid");
+      }
+    }
+  }
+
+  validateForm(){
+    return true;
+  }
 }
