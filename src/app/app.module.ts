a8e4a57fbea95ed617672870413a9fdb25ae5f76
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-07 17:52:16 +0530

    Add left-panel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {LeftPanelComponent} from './auth/components/left-panel/left-panel.component';
+import {SignInComponent} from './auth/components/sign-in/sign-in.component';
+import {SignUpComponent} from './auth/components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftPanelComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LeftPanelComponent } from './components/left-panel/left-panel.component';
-import { SignInComponent } from './components/sign-in/sign-in.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
-
 
 @NgModule({
   declarations: [
-    LeftPanelComponent,
-    SignInComponent,
-    SignUpComponent
+
   ],
   imports: [
     CommonModule,
