8fb68423a98209ae9ad5efd74d81f59b287506d8
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-08 12:09:37 +0530

    finalize the nav bar component

--- src/app/dashboard/navbar/navbar.component.ts
+++ src/app/dashboard/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../../service/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +8,43 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  userName = '';
 
+  constructor(private router: Router, private authService: AuthService) {
+  }
+
+  goToSettings() {
+    this.router.navigate(['/dashboard/setting']);
+  }
+
+  ngOnInit() {
+    this.getUserDetails();
+  }
+
+  getUserDetails(){
+    this.authService.getUserDetails().subscribe({
+        next: (data) =>{
+          if(data){
+            this.userName = data.firstName + ' ' + data.lastName;
+          }
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      }
+    )
+  }
+
+  logout() {
+    this.authService.logout().subscribe(
+      {
+        next: res => {
+          console.log(res);
+          localStorage.removeItem('user');
+          this.router.navigate(['']);
+        },
+        error: err => console.log(err)
+      });
+  }
 }
