5f1b90a4d99a3d6d7261f70e06ca3c93c787255c
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-08 11:56:27 +0530

    Impl the home component calculations

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Transaction} from '../../transaction';
+import {IncomeExpenseService} from '../../service/income-expense.service';
+import {NotificationService} from '../../service/notification.service';
 
 @Component({
   selector: 'app-home',
@@ -13,13 +15,56 @@ export class HomeComponent implements OnInit{
   totalIncome = 0;
   totalLoan = 0;
   totalSavings = 0;
-  percentageValue = 0;
 
-  constructor() {
+  constructor(private incomeExpenseService: IncomeExpenseService, private notificationService: NotificationService) {
   }
 
   ngOnInit() {
-
+    this.incomeExpenseService.getIncomeExpenseData().subscribe(
+      {
+        next: data => {
+          this.incomeExpenseData = data;
+          this.calculateTotals();
+        },
+        error: err => {
+          console.log(err);
+          this.notificationService.showNotification("Failed to load your data, retry again", "error");
+        }
+      });
   }
 
+  calculateTotals(){
+    this.totalIncome = this.calculateTotalByType('INCOME');
+    this.totalExpenses = this.calculateTotalByType('EXPENSE');
+    this.totalLoan = this.calculateTotalByKeyword('loan');
+    this.totalSavings = this.totalIncome - this.totalExpenses; // only for display service
+  }
+
+  calculateTotalByType(type: string): number {
+    return this.incomeExpenseData
+      .filter(transaction => transaction.type == type)
+      .reduce((acc, t) => acc + t.amount, 0);
+  }
+
+  calculateTotalByKeyword(keyword: string): number {
+    return this.incomeExpenseData
+      .filter(transaction => transaction.description.toLowerCase().includes(keyword))
+      .reduce((acc, t) => acc + t.amount, 0);
+  }
+
+  percentageOfLoan(): number {
+    return this.totalIncome ? (this.totalLoan / this.totalIncome) * 100 : 0;
+  }
+
+  percentageOfSavings(): number {
+    return this.totalIncome ? (this.totalSavings / this.totalIncome) * 100 : 0;
+  }
+
+  percentageOfIncomeUsed(): number {
+    return this.totalIncome !== 0 ? (this.totalSavings / this.totalIncome) * 100 : 0;
+  }
+
+  percentageOfExpense(): number {
+    return this.totalIncome !== 0 ? (this.totalExpenses / this.totalIncome) * 100 : 0;
+  }
 }
